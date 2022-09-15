7d46f1cced50e6e02da76bc5c6e23b25b09e41ba
tarachom <tarachom@i.ua>
2022-09-15 20:59:11 +0300

    Add project files.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxInfo = new System.Windows.Forms.RichTextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonDownloadExCurr = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxInfo
+            // 
+            this.richTextBoxInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxInfo.BackColor = System.Drawing.SystemColors.Info;
+            this.richTextBoxInfo.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.richTextBoxInfo.Location = new System.Drawing.Point(8, 41);
+            this.richTextBoxInfo.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.richTextBoxInfo.Name = "richTextBoxInfo";
+            this.richTextBoxInfo.ReadOnly = true;
+            this.richTextBoxInfo.Size = new System.Drawing.Size(787, 486);
+            this.richTextBoxInfo.TabIndex = 2;
+            this.richTextBoxInfo.Text = "";
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Enabled = false;
+            this.buttonCancel.Location = new System.Drawing.Point(285, 4);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(113, 31);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Зупинити";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // buttonDownloadExCurr
+            // 
+            this.buttonDownloadExCurr.Location = new System.Drawing.Point(8, 4);
+            this.buttonDownloadExCurr.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonDownloadExCurr.Name = "buttonDownloadExCurr";
+            this.buttonDownloadExCurr.Size = new System.Drawing.Size(106, 31);
+            this.buttonDownloadExCurr.TabIndex = 3;
+            this.buttonDownloadExCurr.Text = "Завантажити";
+            this.buttonDownloadExCurr.UseVisualStyleBackColor = true;
+            this.buttonDownloadExCurr.Click += new System.EventHandler(this.buttonDownloadExCurr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 539);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonDownloadExCurr);
+            this.Controls.Add(this.richTextBoxInfo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBoxInfo;
+        private Button buttonCancel;
+        private Button buttonDownloadExCurr;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,95 @@
+﻿
+using System.Diagnostics;
+using System.Threading;
+using System.Xml;
+using System.IO;
+using System.Xml.XPath;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        CancellationTokenSource CancellationTokenThread { get; set; }
+        Thread thread { get; set; }
+
+        private void buttonDownloadExCurr_Click(object sender, EventArgs e)
+        {
+            CancellationTokenThread = new CancellationTokenSource();
+            thread = new Thread(new ThreadStart(DownloadExCurr));
+            thread.Start();
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            CancellationTokenThread.Cancel();
+        }
+
+        void DownloadExCurr()
+        {
+            bool isOK = false;
+
+            ApendLine("Завантаження ХМЛ файлу з курсами валют з офційного сайту: bank.gov.ua");
+            ApendLine(" --> https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+
+            XPathDocument xPathDoc;
+            XPathNavigator? xPathDocNavigator = null;
+
+            try
+            {
+                xPathDoc = new XPathDocument("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+                xPathDocNavigator = xPathDoc.CreateNavigator();
+
+                isOK = true;
+                ApendLine("OK\n");
+            }
+            catch (Exception ex)
+            {
+                ApendLine("Помилка завантаження або аналізу ХМЛ файлу: " + ex.Message);
+            }
+
+            if (isOK)
+            {
+                XPathNodeIterator? КурсВалюти = xPathDocNavigator?.Select("/exchange/currency");
+                while (КурсВалюти!.MoveNext())
+                {
+                    if (CancellationTokenThread.IsCancellationRequested)
+                        break;
+
+                    XPathNavigator? current = КурсВалюти.Current;
+
+                    string Код_R030 = int.Parse(current?.SelectSingleNode("r030")?.Value ?? "0").ToString("D3");
+                    string НазваВалюти = current?.SelectSingleNode("txt")?.Value ?? "";
+                    string Коротко = current?.SelectSingleNode("cc")?.Value ?? "";
+                    decimal Курс = decimal.Parse(current?.SelectSingleNode("rate")?.Value.Replace(".", ",") ?? "0");
+                    DateTime ДатаКурсу = DateTime.Parse(current?.SelectSingleNode("exchangedate")?.Value ?? DateTime.MinValue.ToString());
+
+                    ApendLine($"Код {Код_R030}, назва валюти {НазваВалюти}, коротка назва {Коротко}, курс {Курс}, дата курсу {ДатаКурсу}");
+                }
+            }
+
+            if (!this.Disposing)
+            {
+                buttonDownloadExCurr.Invoke(new Action(() => buttonDownloadExCurr.Enabled = true));
+                buttonCancel.Invoke(new Action(() => buttonCancel.Enabled = false));
+            }
+        }
+
+        private void ApendLine(string head)
+        {
+            if (richTextBoxInfo.InvokeRequired)
+            {
+                richTextBoxInfo.Invoke(new Action<string>(ApendLine), head);
+            }
+            else
+            {
+                richTextBoxInfo.AppendText("\n" + head);
+            }
+        }
+
+    }
+}
